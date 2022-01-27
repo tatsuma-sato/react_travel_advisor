006fbf2944a1e70613841612f4fd9c2e175afca6
tatsuma-sato <t.tsuma.s@gmail.com>
2022-01-26 17:14:51 -0800

    added place details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
+
+import { getPlacesData } from "./Api";
 
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
@@ -7,16 +9,41 @@ import Map from "./components/Map/Map";
 import PlaceDetails from "./components/PlaceDetails/PlaceDetails";
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    // setTimeout(() => {
+      getPlacesData(bounds.sw, bounds.ne).then((data) => {
+        setPlaces(data);
+      });
+    // }, 3000);
+  }, [coordinates, bounds]);
+
   return (
     <>
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+          />
         </Grid>
       </Grid>
       <PlaceDetails />
--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,7 +1,110 @@
 import React from "react";
+import {
+  Box,
+  Typography,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Chip,
+} from "@material-ui/core";
+import { LocationOn, Phone } from "@material-ui/icons";
+import { PhoneTwoTone } from "@material-ui/icons";
+import { Rating } from "@material-ui/lab";
+
+import useStyles from "./styles";
 
 const PlaceDetails = ({ place }) => {
-  return <h1>{place && place.name}</h1>;
+  const classes = useStyles();
+  if (place !== undefined) {
+    return (
+      <Card elevation={6}>
+        <CardMedia
+          style={{ height: 350 }}
+          image={
+            place.photo
+              ? place.photo.images.large.url
+              : "https://dummyimage.com/300"
+          }
+          title={place.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            {place.name}
+          </Typography>
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1">Price</Typography>
+            <Typography gutterBottom variant="subtitle1">
+              {place.price}
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1">Ranking</Typography>
+            <Typography gutterBottom variant="subtitle1">
+              {place.ranking}
+            </Typography>
+          </Box>
+          {place?.awards?.map((award) => (
+            <Box my={1} display="felx" justifyContent="space-between">
+              <img src={award.images.small} alt={award.display_name} />
+              <Typography variant="subtitle2" color="textSecondary">
+                {award.display_name}
+              </Typography>
+            </Box>
+          ))}
+          {place?.cuisine?.map(({ name }) => (
+            <Chip
+              key={name}
+              size="small"
+              label={name}
+              className={classes.chip}
+            />
+          ))}
+          {place?.address && (
+            <Typography
+              gutterBottom
+              variant="body2"
+              color="textSecondary"
+              className={classes.subtitle}
+            >
+              <LocationOn />
+              {place.address}
+            </Typography>
+          )}
+          {place?.phone && (
+            <Typography
+              gutterBottom
+              variant="body2"
+              color="textSecondary"
+              className={classes.spacing}
+            >
+              <Phone />
+              {place.phone}
+            </Typography>
+          )}
+          <CardActions>
+            <Button
+              size="small"
+              color="primary"
+              onClick={() => window.open(place.web_url, "_brankc")}
+            >
+              Trip Advisor
+            </Button>
+            <Button
+              size="small"
+              color="primary"
+              onClick={() => window.open(place.website, "_brankc")}
+            >
+              Website
+            </Button>
+          </CardActions>
+        </CardContent>
+      </Card>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default PlaceDetails;
